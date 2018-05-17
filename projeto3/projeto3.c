e7ce42747ba16536bdc83f9bf17e33265633e0f9
Victor Gomide <victoragomide@gmail.com>
2018-05-16 22:42:29 -0300

    Criacao de primeiras funcoes com lista

--- projeto3/projeto3.c
+++ projeto3/projeto3.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definições de Tipos de Dados
 typedef struct Contatos {
   char nome[100];
   char telefone[10];
   char endereco[100];
   int cep;
   char datNasc[10];
-  struct Contatos *prox;
-}contato;
+} contato;
 
+typedef struct Lista {
+  struct Lista *anterior;
+  struct Lista *proximo;
+  int conteudo;
+} lista;
+
+// Protótipos de Função
+lista *criaListaVazia();
+void adicionaInicioLista(lista *dupla);
+void imprimeLista(lista *dupla);
+
+// Main
 int main(int argc, char const *argv[]) {
+  lista *dupla;
+  dupla = criaListaVazia();
+  adicionaInicioLista(dupla);
+  adicionaInicioLista(dupla);
+  imprimeLista(dupla);
+  /*
   int opc;
   do {
     printf("*** Menu ***\n");
@@ -33,6 +51,35 @@ int main(int argc, char const *argv[]) {
       // função para visualizar agenda
     }
   } while(opc != 5);
-
+  */
   return 0;
+}
+// Funções
+lista *criaListaVazia() {
+  return NULL;
+}
+
+void adicionaInicioLista(lista *dupla) {
+  lista *novo;
+  novo = (lista *) malloc(sizeof(lista));
+  printf("Insira o conteúdo do novo elemento da lista: ");
+  scanf("%d", &novo->conteudo);
+  novo->anterior = NULL;
+  if(dupla == NULL)
+    novo->proximo = NULL;
+  else {
+    novo->proximo = dupla;
+    dupla->anterior = novo;
+    dupla = novo;
+  }
+}
+
+void imprimeLista(lista *dupla) {
+  lista *aux;
+  aux = NULL;
+  printf("Conteúdo da lista:\n");
+  for(aux = dupla; aux->proximo == NULL; aux = dupla->proximo) {
+    printf("blabla\n");
+    printf("%d ", aux->conteudo);
+  }
 }
