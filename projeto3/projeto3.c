b8f17a01009ea025c97aa56d5c2748a88ee0a82f
Victor Gomide <victoragomide@gmail.com>
2018-05-17 05:33:29 -0300

    Tentativa (frustrada) de resolver problema com leitura de dados

--- projeto3/projeto3.c
+++ projeto3/projeto3.c
@@ -20,7 +20,7 @@ typedef struct Lista {
 lista *criaListaVazia();
 lista *adicionaInicioLista(lista *dupla);
 void imprimeLista(lista *dupla);
-void escreverArquivo(lista *dupla);
+void escreveArquivo(lista *dupla);
 
 // Main
 int main(int argc, char const *argv[]) {
@@ -37,6 +37,23 @@ int main(int argc, char const *argv[]) {
     printf("5) Sair.\n");
     scanf("%d",&opc);
 
+    switch(opc) {
+      case 1:
+        dupla = adicionaInicioLista(dupla);
+        break;
+      case 2:
+        // removeRegistro
+        break;
+      case 3:
+        // buscaRegistro
+        break;
+      case 4:
+        printf("Conteúdo da lista:\n");
+        imprimeLista(dupla);
+    }
+  } while (opc != 5);
+
+/*
     if (opc == 1) {
       dupla = adicionaInicioLista(dupla);
     }
@@ -50,7 +67,9 @@ int main(int argc, char const *argv[]) {
       imprimeLista(dupla);
     }
   } while(opc != 5);
+*/
 
+  escreveArquivo(dupla);
   return 0;
 }
 // Funções
@@ -60,18 +79,31 @@ lista *criaListaVazia() {
 
 lista *adicionaInicioLista(lista *dupla) {
   lista *novo;
-  novo = (lista *) malloc(sizeof(lista));
+  novo = (lista *) calloc(1, sizeof(lista));
   if (novo == NULL) {
     return 0;
   }
-
-  printf("Nome: "); scanf("%s", novo->conteudo.nome);
-  printf("Telefone: "); scanf("%s", novo->conteudo.telefone);
-  printf("Endereco: "); scanf("%s", novo->conteudo.endereco);
+  
+/*
+  printf("Nome: "); fgets(novo->conteudo.nome, 100, stdin);
+  printf("Telefone: "); fgets(novo->conteudo.telefone, 10, stdin);
+  printf("Endereco: "); fgets(novo->conteudo.endereco, 100, stdin);
   printf("CEP: "); scanf("%ld", &novo->conteudo.cep);
+  printf("Data de Nascimento: "); fgets(novo->conteudo.datNasc, 10, stdin);
+*/
+
+  fflush(stdin);
+  printf("Nome: "); scanf("%s", novo->conteudo.nome);
+  fflush(stdin);
+  printf("Telefone: "); scanf("%s", novo->conteudo.telefone);
+  fflush(stdin);
+  printf("Endereco: "); scanf("%s", novo->conteudo.endereco);
+  fflush(stdin);
+  printf("CEP: "); scanf("%ld", &novo->conteudo.cep);
+  fflush(stdin);
   printf("Data de Nascimento: "); scanf("%s", novo->conteudo.datNasc);
 
-  escreverArquivo(novo);
+  //escreveArquivo(novo);
 
   novo->proximo = dupla;
   novo->anterior = NULL;
@@ -101,18 +133,33 @@ void imprimeLista(lista *dupla) {
   }
 }
 
-void escreverArquivo(lista *dupla) {
+void escreveArquivo(lista *dupla) {
+  FILE *arq = fopen("contatos.txt", "w");
+  if (arq == NULL) {
+    printf("Falha");
+    exit(1);
+  }
+  fprintf(arq, "%s\n", dupla->conteudo.nome);
+  fprintf(arq, "%s\n", dupla->conteudo.telefone);
+  fprintf(arq, "%s\n", dupla->conteudo.endereco);
+  fprintf(arq, "%ld\n", dupla->conteudo.cep);
+  fprintf(arq, "%s\n", dupla->conteudo.datNasc);
+  fprintf(arq, "%s\n", "$");
+  if(dupla->proximo != NULL) {
+    escreveArquivo(dupla->proximo);
+  }
+
+/*
   FILE *arq = fopen("contatos.txt", "a+");
   if (arq == NULL) {
     printf("Falha");
     exit(1);
   }
-
     fprintf(arq, "%s\n", dupla->conteudo.nome);
     fprintf(arq, "%s\n", dupla->conteudo.telefone);
     fprintf(arq, "%s\n", dupla->conteudo.endereco);
     fprintf(arq, "%ld\n", dupla->conteudo.cep);
     fprintf(arq, "%s\n", dupla->conteudo.datNasc);
     fprintf(arq, "%s\n", "$");
-
+*/
 }
